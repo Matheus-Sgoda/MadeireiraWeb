bddc79fce47f64c0ccf78d21bedcb64f2c5a1919
Matheus Sgoda <matheussgoda@bne.com.br>
2025-12-02 14:24:33 -0300

    Remoção do methodo de teste do ConexãoBanco.

--- src/main/java/br/edu/ifpr/madeireira/util/ConexaoBanco.java
+++ src/main/java/br/edu/ifpr/madeireira/util/ConexaoBanco.java
@@ -9,7 +9,7 @@ public class ConexaoBanco {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/madeireira_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "";
 
     public static Connection getConexao() {
         try {
@@ -17,17 +17,6 @@ public class ConexaoBanco {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException("Erro ao conectar no banco: ", e);
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            Connection con = getConexao();
-            System.out.println("Banco conectado com sucesso!");
-            con.close();
-        } catch (Exception e) {
-            System.out.println("Falha na conexão.");
-            e.printStackTrace();
         }
     }
 }
